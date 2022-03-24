ac3de7c950731acca92c537b50a576e6d2eb5bbe
Malik8008 <tu60prbs1@code.edu.az>
2022-03-24 11:23:48 +0400

    Select Word completed

---
+++ SelectWord/SelectWord/Extension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelectWord
+{
+    static class Extension
+    {
+        public static bool Selection(this string sentence, string searched)
+        {            
+            int i = 0;
+            foreach (char c in sentence)
+            {
+                if (i != searched.Length)
+                {
+                    if (c == searched[i])
+                    {
+                        i++;
+                    }
+                    else if (c != searched[i])
+                        i = 0;
+                }
+                else break;            
+            }
+            if (i == searched.Length)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ SelectWord/SelectWord/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SelectWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cumleni daxil edin");
+            string word = Console.ReadLine();
+            Console.WriteLine("Axtarmaq istediyiniz sozu yazin");
+            string searched = Console.ReadLine();
+            bool answer = word.Selection(searched);
+            Console.WriteLine(answer) ;
+        }
+    }
+}
